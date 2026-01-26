62f953eb52ed430a555e0d0f6d535279e18e1d78
vrundaaa27 <vrundaaa2704@gmail.com>
2026-01-26 15:10:57 +0530

    Updated Arithmetic and Assignment Operators

---
+++ Lecture 2/script.js
@@ -0,0 +1,83 @@
+//OPERATORS IN JS
+
+//ARITHMETIC
+//ADDITION OPERATOR
+a=5;
+b=6;
+console.log( "a + b = ", a + b);
+
+//SUBTRACTION OPERATOR
+a=5;
+b=6;
+console.log( "a - b = ", a - b);
+
+//MULTIPLICATION OPERATOR
+a=5;
+b=6;
+console.log( "a * b = ", a * b);
+
+//DIVISION OPERATOR
+a=5;
+b=6;
+console.log( "a / b = ", a / b);
+
+//MODULUS OPERATOR
+a=7;
+b=6;
+console.log( "a % b = ", a % b);
+
+//INCREMENT OPERATOR
+a=5;
+b=6;
+// console.log( "a++ = ", a++);
+console.log( "++a = ", ++a);
+// console.log(a);
+
+//DECREMENT OPERATOR
+a=5;
+b=6;
+console.log( "--b = ", --b);
+// console.log(b);
+
+//DECREMENT OPERATOR
+a=5;
+b=3;
+console.log( " a ** b = ", a ** b);
+// console.log(b);
+
+
+//ASSIGNMENT
+// =
+c = 5;
+d = 6;
+console.log("c =", c);
+
+// +=
+c = 5;
+d = 6;
+console.log("c +=", c += 2);
+
+// -=
+c = 5;
+d = 6;
+console.log("c -=", c -= 2);
+
+// *=
+c = 5;
+d = 6;
+console.log("c *=", c *= 2);
+
+// /=
+c = 5;
+d = 6;
+console.log("c /=", c /= 2);
+
+// %=
+c = 5;
+d = 6;
+console.log("c %=", c %= 2);
+
+// **=
+c = 5;
+d = 6;
+console.log("c **=", c **= 2);
