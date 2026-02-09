36bfd124cf8ce2cd80bf943b77aea606ce9e8fa0
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-09 12:12:42 +0530

    Q1 Udated

--- Lecture 2/Practice Questions/scriptQ1.js
+++ Lecture 2/Practice Questions/scriptQ1.js
@@ -4,5 +4,5 @@ let num = prompt("Enter a number: ");
 if(num % 5 === 0){
     console.log(num + " is a multiple of 5");
 }else{
-    console.log(num + " is NOT a multiple of 5");
+    console.log(num + " is NO a multiple of 5");
 }
