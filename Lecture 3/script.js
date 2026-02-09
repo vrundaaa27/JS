91ad487fc1e04e1239f5b9c858016794bd0e3c46
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-09 12:47:31 +0530

    Updated

--- Lecture 3/script.js
+++ Lecture 3/script.js
@@ -3,3 +3,21 @@
 for(let a = 1; a <= 5; a++){
     console.log("Vrunda");
 }
+
+//Calculate the sum of numbers from 1 to 5
+let sum = 0;
+for( let i = 1; i <=5; i++){
+    sum = sum + i;
+    console.log(sum);
+}
+
+console.log("loop ended");    
+
+//Calculate the sum of numbers from 1 to n
+let sum1 = 0;
+let n = 10;
+
+for( let j = 1; j <=10; j++){
+    sum1 = sum1 = j;
+    console.log(sum1);
+}
