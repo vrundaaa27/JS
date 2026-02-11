ed0775fabcf6751bdf33270090dc7a2664671e3c
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-11 11:05:07 +0530

    PS Q1 was added

---
+++ Lecture 3/Practice Questions/Que 1/scriptQ1.js
@@ -0,0 +1,12 @@
+//Print all even numbers from 0 to 100
+
+for(let i = 0; i <= 100; i++){
+    //even
+    // if( i % 2 == 0){
+    //     console.log(i);
+    // }
+    //odd
+    if( i % 2 !== 0){
+        console.log(i);
+    }
+}
