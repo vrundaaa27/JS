19a82dea824d133340496f85b3f4d6a895c9c676
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-11 10:43:19 +0530

    New changes were added

--- Lecture 3/script.js
+++ Lecture 3/script.js
@@ -21,3 +21,49 @@ for( let j = 1; j <=10; j++){
     sum1 = sum1 = j;
     console.log(sum1);
 }
+
+//INFINITE LOOP
+// for(let k = 1; k <=5; k--){
+//     console.log("Vrunda");
+// }
+
+//WHILE LOOP
+let c = 1;
+while(c <=5){
+    console.log("Vrunda");
+    c++;
+}
+
+//DO-WHILE LOOP
+let d = 10;
+do{
+    console.log("Mhaske");
+}while(d < 5);
+
+//FOR-OF LOOP
+let e = "APNACOLLEGE";
+for(let z of e){
+    console.log("z =",z);
+}
+
+//FOR-IN LOOP
+//EG: 1
+let f ="JAVASCRIPT";
+let size = 0;
+for(let y of f){
+    console.log("y =",y);
+    size++;
+}
+console.log("size=",size);
+
+//EG: 2
+let student = {
+    name : "Vrunda Mhaske",
+    age : 21,
+    grade : "TE",
+    cgpa : 8.3,
+}
+for(let key in student){
+    console.log("key =",key, " value =",student[key]);
+}
+
