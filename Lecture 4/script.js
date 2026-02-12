f72a29337935d42d527e751f0934e08614ea66cc
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-12 12:28:11 +0530

    updated

---
+++ Lecture 4/script.js
@@ -0,0 +1,15 @@
+//ARRAYS
+
+let marks = [97, 81 , 75, 92, 88];
+console.log(marks);
+
+//LOOPING OVER AN ARRAY
+
+let cities = ["Pune","Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata"];
+for(let j = 0 ; j < cities.length; j++){
+    console.log(cities[j]);
+}
+
+for( let city of cities){
+    console.log(city.toUpperCase());
+}
