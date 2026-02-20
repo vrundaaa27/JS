ecc5691be07c23d0dc2b7cdb421646e4c85b968d
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-20 21:26:15 +0530

    Array methods added

--- Lecture 4/script.js
+++ Lecture 4/script.js
@@ -13,3 +13,53 @@ for(let j = 0 ; j < cities.length; j++){
 for( let city of cities){
     console.log(city.toUpperCase());
 }
+
+//ARRAY METHODS
+
+//PUSH()
+let items = ["pen", "paper", "ink"];
+console.log(items);
+items.push("eraser","colours");
+console.log(items);
+
+//POP()
+items.pop();
+console.log(items);
+
+//TOSTRING()
+console.log(items);
+console.log(items.toString());
+
+//CONCAT()
+let names = ["Sita", "Gita"];
+let surnames = ["Pawar","Shinde","Deshmukh"];
+let fullNames = names.concat(surnames);
+console.log(fullNames);
+
+//UNSHIFT()
+items.unshift("crayons");
+console.log(items);
+
+//SHIFT()   
+items.shift();
+console.log(items);
+
+//SLICE()
+console.log(items);
+console.log(items.slice(1,4));
+
+//SPLICE()
+let arr = ['1','2','3','4','5','6','7'];
+console.log(arr);
+//ADD ELEMENT
+// arr.splice(2,0,'101');
+// console.log(arr);
+// console.log(arr.length);
+// //REMOVE ELEMENT
+// arr.splice(3,1);
+// console.log(arr);
+// console.log(arr.length);
+// REPLACE ELEMENT
+arr.splice(0,1,'000','101');
+console.log(arr);
+console.log(arr.length);
