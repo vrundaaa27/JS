d00a459a37e3362da6e40db275483b47a0ced22c
vrundaaa27 <vrundaaa2704@gmail.com>
2026-02-20 20:04:35 +0530

    PQ2 added

---
+++ Lecture 4/Practice Questions/Que2/scriptQ2.js
@@ -0,0 +1,18 @@
+let val = [250, 645, 300, 900, 50];
+
+// let index = 0;
+
+// for(let items of val){
+//     console.log(`The value at ${index} = ${val[index]}`);
+//     let offer = val[index]/10;
+//     let discount = val[index]-offer;
+//     console.log(`The value of items after applying offer is ${discount}`);
+//     index++;
+// }
+
+for( let i = 0; i < val.length; i++){
+    console.log(`Value of items at ${i} = ${val[i]}`);
+    let offer = val[i]/10;
+    let discount = val[i] - offer;
+    console.log(`The value of items after applying offer is ${discount}`);
+}
